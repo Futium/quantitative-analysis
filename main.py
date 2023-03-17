0beddb8fb1e76edb65eff6a7e2da88d9ea6d738d
muriatic <64611874+muriatic@users.noreply.github.com>
2023-03-16 21:55:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import yfinance as yf
+import pandas as pd
 
 ticker = input("What Ticker are you seeking analysis on?\n").upper()
 
 data = yf.download(ticker, interval='1m')
+
+df = pd.DataFrame(data)
+
+df[]
+
+df.to_csv('df.csv')
 
 print(data)
 
