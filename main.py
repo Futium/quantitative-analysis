180c165c2113859ab1e2cf168d34273888e54cc4
muriatic <64611874+muriatic@users.noreply.github.com>
2023-03-16 23:07:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 ticker = input("What Ticker are you seeking analysis on?\n").upper()
 
-data = yf.download(ticker, interval='1m')
+data = yf.download(ticker, interval='1m', period='1d', prepost=True)
 
 df = pd.DataFrame(data)
 
-df[]
+df.to_csv(ticker + '.csv')
 
-df.to_csv('df.csv')
+# df = df[''df.Datetime ]
 
-print(data)
+print(df)
 
 print(yf.Ticker(ticker).fast_info['lastPrice'])
