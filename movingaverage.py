5ad52c118b8a4aeb561357baf7beea46521d974b
muriatic <64611874+muriatic@users.noreply.github.com>
2023-03-23 12:48:50 -0400

    Fixed the grading system
    Now it calculates gains from sell - buy.
    
    Removed:
    - trimming the data frame (unnecessary)
    - removed eval_len, no longer used
    - changed .append to .extend in order to continue adding to the list instead of adding a nested list.

--- movingaverage.py
+++ movingaverage.py
@@ -24,9 +24,10 @@ prec_lvl = config.prec_lvl
 csv_save_location = config.csv_save_location
 performance_folder = config.performance_folder
 performance_figures_folder = config.performance_figures_folder
+data_date = config.data_date
 
 # what the file name should be 
-filename = 'k_of_' + str(k) + '_historicalRecord_for_' + ticker + '_on_' + str(today) + '.csv'
+filename = 'k_of_' + str(k) + '_historicalRecord_for_' + ticker + '_on_' + str(data_date) + '.csv'
 
 # grab the data whether historical or present
 df = historical_data.get_historical_data(ticker)
@@ -152,90 +153,66 @@ def get_moving_average(ticker_price, x):
 def evaluate_performance():
     df2 = pd.read_csv(csv_save_location + "/" + filename)
 
-    df2 = df2.iloc[k:]
-    df2 = df2.drop('Unnamed: 0', axis=1)
+    df_new = df2[df2['Action'].notna()]
 
-    desired_action = "BUY"
-    
-    columns_names = []
+    grp = (df_new['Action'] == 'SELL').cumsum().shift().bfill()
 
-    prices = []
+    dd = dict(tuple(df_new.groupby(grp)))
 
-    comparison_point = 0
+    list_of_dfs = [g for _, g in dd.items() if len(g) > 1]
 
-    # create list of column names
-    for n in range(eval_len):
-        columns_names.append('Price ' + str(n))
+    buy_price = []
+    price = []
+    sell_price = []
+    pct_gain = []
+    moving_average = []
+    action = []
+    current_time = []
 
-    # define column names from columns     
-    data = pd.DataFrame(columns=columns_names)
+    ### len of list_of_dfs is # of subtables 
+    for n in range(len(list_of_dfs)):
+        ###(list with all the data[subtable number]['Column Name'].iloc[nth row])
+        buy_price.append(list_of_dfs[n]['Price'].iloc[0])
+        sell_price.append(list_of_dfs[n]['Price'].iloc[-1])
 
-    gain_table = pd.DataFrame()
+        ## get values for performance figures and use extend to add them to the list
+        current_time.extend(list_of_dfs[n]['Current Time'].tolist())
+        price.extend(list_of_dfs[n]['Price'].tolist())
+        moving_average.extend(list_of_dfs[n]['Moving Average'].tolist())
+        action.extend(list_of_dfs[n]['Action'].tolist())
 
-    df2.to_csv('temp32123.csv')
+        pct_gain.append(100 * (sell_price[n] - buy_price[n]) / buy_price[n])
+    total_gain = str(sum(pct_gain)) + '%' 
 
-    # create prices list and then add each array of prices to the column
-    for n in range(eval_len):
-        # get prices for each window each column is all the data for the nth value of the eval_len
-        prices.append([window['Price'].iloc[n] for window in df2.rolling(window=eval_len)
-           if len(window) == eval_len and window['Action'].iloc[0] == desired_action])
-        
-        # set each column equal to the nth price list
-        data[data.columns[n]] = prices[n]
-        
-        if n != 0:
-             # find the names of the price columns that we are looking for
-            nth_column_name = data.columns[n]
-            
-            comparision_column_name = data.columns[comparison_point]
-        
-            # create the name of the column for the specific piece of data
-            key_name = 'P ' + str(n) + ' - P ' + str(comparison_point)
-            data[key_name] = data[nth_column_name] - data[comparision_column_name]
-            
-            # create a table with the gains from all of the stocks
-            gain_table[key_name] = 100 * (data[nth_column_name] - data[comparision_column_name]) / data[comparision_column_name]
-
-        print(data)
-
-    
-    gain_table.to_csv('temp.csv')
-
-    # find maximum of each row  
-    data['Greatest Gain %'] = gain_table.max(axis=1)
-
-    # find the column it occurred in
-    data['Time of Success'] = gain_table.idxmax(axis=1)
-        
-    most_successful_time = data.mode()['Time of Success'][0]
-
-    ### if we always made the right choice:
-    perfect_ttl = str(round(data['Greatest Gain %'].sum(), prec_lvl)) + '%'
-
-    ### total assuming you use the same method every time
-    actual_ttl = str(round(gain_table[key_name].sum(), prec_lvl)) + '%'
-
-    # create performance figures file name
-    performance_figures_file_name = 'performance_figures_with_eval_len_' + str(eval_len) + '_k_of_' + str(k) + '_on_' + str(today) + '.csv'
-
-    # save to csv
-    data.to_csv(os.path.join(performance_figures_folder, performance_figures_file_name))
-    
-    # find the performance figures and make them into a table
-    values = [[most_successful_time, actual_ttl, perfect_ttl]]
-    performance = pd.DataFrame(values, columns=['Most Frequent Row:', 'TTL Based on this Row:', 'Perfect Total:'])
+    ### take the performance and get them into a .csv file
+    # find the performance and make them into a table
+    gain_values = [[total_gain]]
+    performance = pd.DataFrame(gain_values, columns=['Total Gain:'])
 
     # create file name for performance
-    performance_file_name = 'performance_with_for_' + ticker + '_with_eval_len_' + str(eval_len) + '_k_of_' + str(k) + '_on_' + str(today) + '.csv'
+    performance_file_name = 'performance_with_for_' + ticker + '_k_of_' + str(k) + '_on_' + str(data_date) + '.csv'
 
     # save to csv
     performance.to_csv(os.path.join(performance_folder, performance_file_name))
 
-    # pprint()
+    # print(current_time)
+
+    ### take the subtables and make them into a dataframe
+    columns = ['Current Time', 'Price', 'Moving Average', 'Action']
+    data = pd.DataFrame(columns=columns)
+
+    ### set the columns = to the lists
+    data['Current Time'] = current_time
+    data['Price'] = price
+    data['Moving Average'] = moving_average
+    data['Action'] = action
+
+    # create performance figures file name
+    performance_figures_file_name = 'performance_figures_with_eval_len_' + '_k_of_' + str(k) + '_on_' + str(data_date) + '.csv'
+
+    # save to csv
+    data.to_csv(os.path.join(performance_figures_folder, performance_figures_file_name))
 
 
 if __name__ == "__main__":
     main()
-
-# df.to_csv(ticker + '.csv')
-
