42b78cd3ca6d1f3fd0a4eab43fc7991682b33e5f
muriatic <64611874+muriatic@users.noreply.github.com>
2023-04-04 16:30:55 -0400

    optimization #1.1

--- main.py
+++ main.py
@@ -18,12 +18,5 @@ for k in range(1, iterations+1):
         for ticker in ticker_list:
             total_stock_gain.append(moving_avg((ticker), k))
         evaluate.ttl_performance(ticker_list, k)
-
-### no longer using this bc the if stmts could be slowing it down
-# if config.auto == False:
-#     ### get historical data
-#     ticker = input("What Ticker are you seeking analysis on?\n").upper()
-
-# else:
     
 print(time() - start_time)
